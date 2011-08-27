acf25c3659836ff05fa3e94994327c263f2325b8
Uiri Noyb <uiri00@gmail.com>
2011-08-27 13:46:07 -0400

    initial listview stuffs, only run roster_handler once.

--- chatgui.py
+++ chatgui.py
@@ -12,14 +12,14 @@ def send_message_gui(first, second, third, fourth):
     xmpp_client.send_message(third, fourth)
 
 def print_roster(client):
-    while(1):
-        time.sleep(7)
-        print client.roster_handler()
+    time.sleep(7)
+    client.roster_handler()
 
 mainwindow = gtk.Window()
 mainwindow.set_default_size(500,500)
 mainwindow.connect("delete_event", delete_event)
 mainwindow.connect("destroy", destroy_window)
+mainbox = gtk.HBox(False, 0)
 chatbox = gtk.VBox(False, 0)
 mainscroll = gtk.ScrolledWindow()
 mainbuffer = gtk.TextBuffer()
@@ -27,6 +27,14 @@ mainview = gtk.TextView(mainbuffer)
 chathbox = gtk.HBox(False, 0)
 mainmessage = gtk.Entry()
 mainbutton = gtk.Button('Send')
+
+rosterstore = gtk.TreeStore(gobject.TYPE_STRING)
+rosterview = gtk.TreeView(rosterstore)
+rosterscroll = gtk.ScrolledWindow(None, None)
+rosterscroll.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+rosterscroll.add(rosterview)
+rostercell = gtk.CellRendererText()
+
 
 client = fbchatlib.get_facebook_client()
 xmpp_client = fbchatlib.setup_chat(client, mainbuffer)
@@ -41,7 +49,9 @@ mainscroll.add(mainview)
 mainscroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 chatbox.pack_start(mainscroll, True, True, 0)
 chatbox.pack_start(chathbox, False, False, 0)
-mainwindow.add(chatbox)
+mainbox.pack_start(rosterscroll, True, False, 0)
+mainbox.pack_start(chatbox, True, True, 0)
+mainwindow.add(mainbox)
 mainwindow.show_all()
 loopthread = threading.Thread(target=xmpp_client.connect_and_loop)
 loopthread.daemon = True
