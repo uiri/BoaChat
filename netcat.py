ddec854acbcce7c2d08ae4f0f5d81297f9af641b
Uiri Noyb <uiri00@gmail.com>
2011-08-27 16:33:55 -0400

    Added Copyright notice. Copyright 2011 Uiri Noyb and Rafael Khan. Added notice putting the program under the GNU GPL v3 or later. Added copy of GPLv3.

--- netcat.py
+++
@@ -1,7 +0,0 @@
-import fbchatlib, sys
-
-print "Put in the UID of who you want to talk to:"
-uid = sys.stdin.readline()
-client = fbchatlib.get_facebook_client()
-xmpp_client = fbchatlib.setup_chat(client, uid.strip(), 'lol')
-xmpp_client.connect_and_loop()
